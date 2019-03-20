8a1343a03db297225ae1b00906033471cc47af46
Stephan Lorenzen <lorenzen@di.ku.dk>
2019-03-20 21:52:16 +0100

    experiment.py now creates output files

--- experiment.py
+++ experiment.py
@@ -1,13 +1,20 @@
+import sys
+import os
+
 from rfb import RandomForestWithBounds as RFWB
 from rfb import data as mldata
 
+inpath  = 'data/'
+outpath = 'out/'
+
 DATA_SETS = [
         ('Letter:AB',100),
-        ('Letter:DO',100),
-        ('Letter:OQ',100),
-        ('ILPD',     100),
-        ('Mushroom', 100),
-        ('Credit-A', 100)]
+        #('Letter:DO',100),
+        #('Letter:OQ',100),
+        #('ILPD',     100),
+        #('Mushroom', 100),
+        ('Credit-A', 100)
+        ]
 
 bag_results    = []
 val_results    = []
@@ -15,7 +22,7 @@ bagval_results = []
 for dataset,m in DATA_SETS:
     print("##### "+dataset+" #####")
     
-    X,Y = mldata.load(dataset)
+    X,Y = mldata.load(dataset, path=inpath)
 
     trainX,trainY,testX,testY = mldata.split(X,Y,0.5)
     
@@ -38,4 +45,19 @@ for dataset,m in DATA_SETS:
     _, bounds = rf.predict_all(valX,valY)
     val_results.append((details['risk_mv'], bounds))
     
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+def _write_outfile(name, results):
+    prec = 5
+    with open(outpath+name+'.csv', 'w') as f:
+        f.write('dataset;m;risk_mv;pbkl;c1;c2;sh\n')
+        for (ds,m), (risk_mv, bounds) in zip(DATA_SETS, results):
+            f.write(ds+';'+str(m)+';'+
+                    (';'.join(['{'+str(i)+':.'+str(prec)+'f}' for i in range(5)])+'\n')
+                    .format(risk_mv, bounds['PBkl'], bounds['C1'], bounds['C2'], bounds['SH'])
+                    )
+
+_write_outfile('bag-results', bag_results)
+_write_outfile('val-results', val_results)
+_write_outfile('bagval-results', bagval_results)
 
