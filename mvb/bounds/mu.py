fcdba6918033656a23dc1d42eadd4a0fb7a9790d
Stephan Lorenzen <lorenzen@di.ku.dk>
2020-12-29 16:09:43 +0100

    Union bound in mu.py when providing more than one value

--- mvb/bounds/mu.py
+++ mvb/bounds/mu.py
@@ -13,17 +13,21 @@ def MU(tandem_risk, gibbs_risk, n, n2, KL, mu_grid=[0.0], delta=0.05):
     if len(mu_grid)<1:
         return 1.0
 
-    rhs_tr = ( 2.0*KL + log(4.0*sqrt(n2)/delta) ) / n2
+    # Union bound over K = len(mu_grid) -> delta = delta/K
+    K = len(mu_grid)
+    delta /= K
+
     # UpperBound_TandemRisk
+    rhs_tr = ( 2.0*KL + log(4.0*sqrt(n2)/delta) ) / n2
     ub_tr  = solve_kl_sup(tandem_risk, rhs_tr)
     
-    rhs_gr = ( KL + log(4.0*sqrt(n)/delta) ) / n
     # LowerBound_GibbsRisk
+    rhs_gr = ( KL + log(4.0*sqrt(n)/delta) ) / n
     lb_gr  = solve_kl_inf(gibbs_risk, rhs_gr)
    
-    # Compute for mu_grid
-    bnd = 0.0
+    # Compute K bounds
+    bnds = []
     for mu in mu_grid:
-        bnd += 4*(ub_tr - 2*mu*lb_gr + mu**2)
-    return min(1.0, bnd)
+        bnds.append(4*(ub_tr - 2*mu*lb_gr + mu**2))
+    return min(1.0, min(bnds))
 
