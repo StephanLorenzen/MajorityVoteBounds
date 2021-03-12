5e8629fedf040748ef9b32f8bb1d94513f8cce88
YiShanAngWu <lrc379@CB16108.science.domain>
2021-03-12 14:52:22 +0100

    Add run_muBernstein.sh

---
+++ NeurIPS2021/helpers/run_muBernstein.sh
@@ -0,0 +1,9 @@
+M=100
+R=10
+
+python muBernstein.py Phishing      $M $1 $R
+python muBernstein.py Mushroom      $M $1 $R
+python muBernstein.py Pendigits     $M $1 $R
+python muBernstein.py Letter        $M $1 $R
+python muBernstein.py Sensorless    $M $1 $R
+python muBernstein.py Cod-RNA       $M $1 $R
