56df5fdfa470d08f869f0b69a95560cccdbaf3ab
YiShanAngWu <lrc379@CB16108.science.domain>
2021-06-03 14:55:27 +0200

    merge optimize.py and optimize_mce.py

--- NeurIPS2021/helpers/run_optimize.sh
+++ NeurIPS2021/helpers/run_optimize.sh
@@ -1,28 +1,22 @@
-M=100
 O=iRProp
 R=5
 
 
-python optimize.py SVMGuide1     $M $1 $O $R $2
-
-python optimize.py Phishing      $M $1 $O $R $2
-
-python optimize.py Mushroom      $M $1 $O $R $2
-python optimize.py Splice        $M $1 $O $R $2
-
-python optimize.py w1a           $M $1 $O $R $2
-
-python optimize.py Cod-RNA       $M $1 $O $R $2
-
-python optimize.py Adult         $M $1 $O $R $2
-python optimize.py Protein       $M $1 $O $R $2
-python optimize.py Connect-4     $M $1 $O $R $2
-python optimize.py Shuttle       $M $1 $O $R $2
-python optimize.py Pendigits     $M $1 $O $R $2
-python optimize.py Letter        $M $1 $O $R $2
-python optimize.py SatImage      $M $1 $O $R $2
-python optimize.py Sensorless    $M $1 $O $R $2
-python optimize.py USPS          $M $1 $O $R $2
-python optimize.py MNIST         $M $1 $O $R $2
-python optimize.py Fashion-MNIST $M $1 $O $R $2
+python optimize.py SVMGuide1     $1 $2 $3 $O $R
+python optimize.py Phishing      $1 $2 $3 $O $R
+python optimize.py Mushroom      $1 $2 $3 $O $R
+python optimize.py Splice        $1 $2 $3 $O $R
+python optimize.py w1a           $1 $2 $3 $O $R
+python optimize.py Cod-RNA       $1 $2 $3 $O $R
+python optimize.py Adult         $1 $2 $3 $O $R
+python optimize.py Protein       $1 $2 $3 $O $R
+python optimize.py Connect-4     $1 $2 $3 $O $R
+python optimize.py Shuttle       $1 $2 $3 $O $R
+python optimize.py Pendigits     $1 $2 $3 $O $R
+python optimize.py Letter        $1 $2 $3 $O $R
+python optimize.py SatImage      $1 $2 $3 $O $R
+python optimize.py Sensorless    $1 $2 $3 $O $R
+python optimize.py USPS          $1 $2 $3 $O $R
+python optimize.py MNIST         $1 $2 $3 $O $R
+python optimize.py Fashion-MNIST $1 $2 $3 $O $R
 
--- NeurIPS2021/optimize.py
+++ NeurIPS2021/optimize.py
@@ -1,12 +1,17 @@
 #
 # Runs optimization experiments.
 #
-# Usage: python optimize.py <data_set> [M] [sample_mode] [optimizer] [repeats]
+# Usage: python optimize.py <data_set> [M] [base_clf] [sample_mode] [optimizer] [repeats]
 #        M           : number of trees
-#        sample_mode : 'bootstrap', 'dim', f in [0,1]
-#                      'bootstrap' = full bagging.
-#                      'dim'       = sample d points with replacement
-#                      float f     = sample f*|S| points with replacement
+#        base_clf    : 'rfc', 'abc', 'mce'
+#                      'rfc'        = random forest, with fully-grown decision trees
+#                      'abc'        = adaboost, with decision stumps
+#                      'mce'        = multiple heterogeneous classifier, defined in mvb/mce.py
+#        sample_mode : 'bootstrap', 'dim', f in [0,1], 'boost-(int)'
+#                      'bootstrap'  = full bagging.
+#                      'dim'        = sample d points with replacement
+#                      float f      = sample f*|S| points with replacement
+#                      'boost-(int)'= boosting with data divided into (int) splits 
 #        optimizer   : CMA, GD, RProp, iRProp (default)
 #        repeats     : integer
 #
@@ -20,6 +25,7 @@ from sklearn.utils import check_random_state
 from mvb import RandomForestClassifier as RFC
 from mvb import OurAdaBoostClassifier as OABC
 from mvb import BaseAdaBoostClassifier as baseABC
+from mvb import MultiClassifierEnsemble as MCS
 from mvb import data as mldata
 
 if len(sys.argv) < 2:
@@ -27,43 +33,29 @@ if len(sys.argv) < 2:
     sys.exit(1)
 DATASET = sys.argv[1]
 M       = int(sys.argv[2]) if len(sys.argv)>=3 else 100
-SMODE   = sys.argv[3] if len(sys.argv)>=4 else 'bootstrap'
-SMODE   = SMODE if (SMODE=='bootstrap' or SMODE=='dim' or SMODE=='boost') else float(SMODE)
-OPT     = sys.argv[4] if len(sys.argv)>=5 else 'iRProp'
-REPS    = int(sys.argv[5]) if len(sys.argv)>=6 else 1
-SPLITS  = int(sys.argv[6]) if len(sys.argv)>=7 else 2 # number of splits for boosting
+BASE    = sys.argv[3] if len(sys.argv)>=4 else 'rfc'
+SMODE   = sys.argv[4] if len(sys.argv)>=5 else 'bootstrap'
+SMODE   = SMODE if (SMODE=='bootstrap' or SMODE=='dim' or 'boost' in SMODE) else float(SMODE)
+(SMODE,SPLITS) = ('boost', int(SMODE[-1])) if 'boost' in SMODE else (SMODE, None)
+OPT     = sys.argv[5] if len(sys.argv)>=6 else 'iRProp'
+REPS    = int(sys.argv[6]) if len(sys.argv)>=7 else 1
+if BASE is ('rfc' or 'abc' or 'mce'):
+    print("No such base classifier")
+    sys.exit(1)
 
 inpath  = 'data/'
-outpath = 'out/optimize/'
+outpath = 'out/optimize/'+BASE+'/'
 
 SEED = 1000
 
-ADA_DATASET = [
-        'Protein',
-        'Connect-4',
-        'Shuttle',
-        'Pendigits',
-        'Letter',
-        'SatImage',
-        'Sensorless',
-        'USPS',
-        'MNIST',
-        'Fashion-MNIST',
-]
-
-if (SMODE == 'boost' and DATASET in ADA_DATASET):
+if SMODE == 'boost':
     sys.exit(1)
 
 def _write_dist_file(name, rhos, risks):
     with open(outpath+name+'.csv', 'w') as f:
-        if SMODE == 'boost':
-            f.write("h;risk;rho_prior;rho_lam;rho_tnd;rho_mu;rho_bern\n")
-            for i,(err,r_prior,r_lam,r_tnd,r_mu,r_bern) in enumerate(zip(risks, rhos[0], rhos[1], rhos[2], rhos[3], rhos[4])):
-                f.write(str(i+1)+";"+str(err)+";"+str(r_prior)+";"+str(r_lam)+";"+str(r_tnd)+";"+str(r_mu)+";"+str(r_bern)+"\n")
-        else:
-            f.write("h;risk;rho_lam;rho_tnd;rho_mu;rho_bern\n")
-            for i,(err,r_lam,r_tnd,r_mu,r_bern) in enumerate(zip(risks, rhos[0], rhos[1], rhos[2], rhos[3])):
-                f.write(str(i+1)+";"+str(err)+";"+str(r_lam)+";"+str(r_tnd)+";"+str(r_mu)+";"+str(r_bern)+"\n")        
+        f.write("h;risk;rho_lam;rho_tnd;rho_mu;rho_bern\n")
+        for i,(err,r_lam,r_tnd,r_mu,r_bern) in enumerate(zip(risks, rhos[0], rhos[1], rhos[2], rhos[3])):
+            f.write(str(i+1)+";"+str(err)+";"+str(r_lam)+";"+str(r_tnd)+";"+str(r_mu)+";"+str(r_bern)+"\n")        
 
 if not os.path.exists(outpath):
     os.makedirs(outpath)
@@ -73,12 +65,12 @@ def _write_outfile(results):
     prec = 5
     with open(outpath+DATASET+'-'+str(M)+'-'+str(SMODE)+'-'+str(OPT)+'.csv', 'w') as f:
         # Header
-        f.write('repeat;n_train;n_test;d;c;prior_mv_risk')
-        for name in ["ada","unf","lam","tnd","mu","bern"]:
+        f.write('repeat;n_train;n_test;d;c')
+        for name in ["best","unf","lam","tnd","mu","bern"]:
             f.write(';'+';'.join([name+'_'+x for x in ['mv_risk', 'gibbs', 'tandem', 'sh', 'pbkl', 'tnd', 'TandemUB', 'MU', 'ub_tr', 'lb_gr', 'muTandemUB', 'bern', 'mutandem_risk', 'vartandem_risk', 'KL', 'varUB', 'bernTandemUB', 'bl', 'bg', 'bmu']]))
         f.write('\n')
-        for (rep, n, prior, restup) in results:
-            f.write(str(rep+1)+';'+str(n[0])+';'+str(n[1])+';'+str(n[2])+';'+str(n[3])+';'+str(prior));
+        for (rep, n, restup) in results:
+            f.write(str(rep+1)+';'+str(n[0])+';'+str(n[1])+';'+str(n[2])+';'+str(n[3]));
             for (mv_risk, stats, bounds, bl, bg, bm) in restup:
                 f.write(
                         (';'+';'.join(['{'+str(i)+':.'+str(prec)+'f}' for i in range(20)]))
@@ -117,7 +109,7 @@ elif SMODE=='boost':
     smodename = 'boosting with ' + str(SPLITS) + ' splits'
 else:
     smodename = 'reduced bagging ('+str(SMODE)+');'
-print("Starting RFC optimization (m = "+str(M)+") for ["+DATASET+"] using sampling strategy: "+smodename+", optimizer = "+str(OPT))
+print("Starting "+BASE.upper()+ " optimization (m = "+str(M)+") for ["+DATASET+"] using sampling strategy: "+smodename+", optimizer = "+str(OPT))
 results = []
 X,Y = mldata.load(DATASET, path=inpath)
 C = np.unique(Y).shape[0]
@@ -134,40 +126,35 @@ for rep in range(REPS):
     n = (trainX.shape[0], testX.shape[0], trainX.shape[1], C)
 
     # Prepare base classifiers for PAC-Bayes methods
-    rf = RFC(M,max_features="sqrt",random_state=RAND, sample_mode=SMODE)
-    if SMODE == 'boost':
+    if BASE == 'rfc':
+        rf = RFC(M,max_features="sqrt",random_state=RAND, sample_mode=SMODE)
+    elif BASE == 'mce':
+        rf = MCS(n_estimators=M, random_state=RAND, sample_mode=SMODE)
+    else:
         max_depth = 1
-        use_ada_prior = True
-        rf = OABC(n_estimators=M, max_depth=max_depth, random_state=RAND, sample_mode=SMODE, n_splits = SPLITS, use_ada_prior=use_ada_prior)
-
+        rf = OABC(n_estimators=M, max_depth=max_depth, random_state=RAND, sample_mode=SMODE, n_splits = SPLITS, use_ada_prior=True)
+        abc = baseABC(n_estimators=M, max_depth=max_depth, random_state=RAND, n_splits = SPLITS)
+        
     # Training
     print("Training...")    
     _ = rf.fit(trainX,trainY)
     
-    """ AdaBoost related """
-    prior_mv_risk = -1
-    res_ada = (-1, dict(), dict(), -1, -1, -1)
-    
-    if SMODE == 'boost':
-        pi = rf.optimize_rho('AdaBoost')
-        _, prior_mv_risk = rf.predict(testX, testY)
-        rhos.append(pi)
-
-        # Adaboost Baseline
-        print("Calculate the baseline by AdaBoost...")
-        abc = baseABC(n_estimators=M, max_depth=max_depth, random_state=RAND, n_splits = SPLITS)
-        _ = abc.fit(trainX, trainY)
-        _, mv_risk = abc.predict(testX, testY)
-        bounds, stats = abc.bound()
-        res_ada = (mv_risk, stats, bounds, -1, -1, -1)
-    print('prior_mv_risk', prior_mv_risk, 'res_ada_risk', res_ada[0])
+    """ Especially for MCE """
+    print('Best model in the ensemble...')
+    _ = rf.optimize_rho('Best')
+    _, mv_risk = rf.predict(testX,testY)
+    stats  = rf.stats()
+    stats = rf.aggregate_stats(stats) # initial stats after training
+    bounds, stats = rf.bounds(stats=stats)
+    res_best = (mv_risk, stats, bounds, -1, -1, -1)
+    print('mv_risk', mv_risk)
     
     """ Uniform Weighting """
     print('Uniform weighting...')
     _ = rf.optimize_rho('Uniform')
     _, mv_risk = rf.predict(testX,testY)
-    stats = rf.stats(options = {'mu_kl':mu_range,'mu_bern': mu_range}) # initial stats after training
-    bounds, stats = rf.bounds(stats=stats) # compute the bounds according to the best mu in the range, and record the corresponding stats
+    stats = rf.aggregate_stats(stats)
+    bounds, stats = rf.bounds(stats=stats)
     res_unf = (mv_risk, stats, bounds, -1, -1, -1)
     print('mv_risk', mv_risk)
       
@@ -199,7 +186,6 @@ for rep in range(REPS):
     stats = rf.aggregate_stats(stats, options={'mu_kl':(bmu,)}) # update rho-dependent stats
     bounds, stats = rf.bounds(stats=stats) # compute the bounds and the stats with the above mus
     res_MU = (mv_risk, stats, bounds, bl, bg, bmu)
-    print('C\muTND bound: gamma, ', bg, 'lambda', bl, 'mu', bmu)
     rhos.append(rho)
     print('mv_risk', mv_risk)
     
@@ -211,7 +197,6 @@ for rep in range(REPS):
     stats = rf.aggregate_stats(stats, options={'mu_bern':(bmu,), 'lam':bl, 'gam': bg}) # update rho-dependent stats
     bounds, stats = rf.bounds(stats=stats) # compute the bounds and the stats with the above mus
     res_Bern = (mv_risk, stats, bounds, bl, bg, bmu)
-    print('COTND bound: gamma, ', bg, 'lambda', bl, 'mu', bmu)
     rhos.append(rho)
     print('mv_risk', mv_risk)
      
@@ -220,7 +205,7 @@ for rep in range(REPS):
         # record the \rho distribution by all optimization methods
         _write_dist_file('rho-'+DATASET, rhos, stats['risks'])
 
-    results.append((rep, n, prior_mv_risk, (res_ada, res_unf, res_lam, res_tnd, res_MU, res_Bern)))
+    results.append((rep, n, (res_best, res_unf, res_lam, res_tnd, res_MU, res_Bern)))
 
 _write_outfile(results)
 
