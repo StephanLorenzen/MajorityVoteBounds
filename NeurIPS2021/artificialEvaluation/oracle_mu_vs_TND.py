af17dae73698a1e42b8768dbad636beb712d31f0
YiShanAngWu <lrc379@CB16108.science.domain>
2021-06-03 17:00:13 +0200

    clean up artificialEvaluation

--- NeurIPS2021/artificialEvaluation/oracle_mu_vs_TND.py
+++ NeurIPS2021/artificialEvaluation/oracle_mu_vs_TND.py
@@ -3,63 +3,31 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 import matplotlib
 
-title = 'oracle_'
-
 npoints = 1000
 
-expected_gibbs_loss = np.linspace(0.005, 0.495, npoints)
-expected_tandem_loss = np.linspace(0.005, 0.495, npoints)
+gibbs = np.linspace(0.005, 0.495, npoints)
+tandem = np.linspace(0.005, 0.495, npoints)
 
-grid_gibbs_loss, grid_tandem_loss = np.meshgrid(expected_gibbs_loss, expected_tandem_loss)
+grid_gibbs, grid_tandem = np.meshgrid(gibbs, tandem)
 
-mu_star = (0.5 * grid_gibbs_loss - grid_tandem_loss) / (0.5 - grid_gibbs_loss)
-secondOrderBound = 4 * grid_tandem_loss
-c2Bound = (grid_tandem_loss - 2 * mu_star * grid_gibbs_loss + mu_star ** 2) / (0.5 - mu_star) ** 2
+mu_star = (0.5 * grid_gibbs - grid_tandem) / (0.5 - grid_gibbs)
+TND = 4 * grid_tandem
+CmuTND = (grid_tandem - 2 * mu_star * grid_gibbs + mu_star ** 2) / (0.5 - mu_star) ** 2
 
 for i in range(npoints):
     for j in range(npoints):
-        # if Tandem>Gibbs
-        if (grid_tandem_loss[i, j]>grid_gibbs_loss[i, j]):
+        # if Tandem > Gibbs
+        if (grid_tandem[i, j]>grid_gibbs[i, j]):
             mu_star[i,j]=np.nan
-            secondOrderBound[i,j]=np.nan
-            c2Bound[i,j]=np.nan
+            TND[i,j]=np.nan
+            CmuTND[i,j]=np.nan
 
-        # if Gibbs^^>Tandem
-        if (grid_gibbs_loss[i, j]**2>grid_tandem_loss[i, j]):
+        # if Gibbs**2 > Tandem
+        if (grid_gibbs[i, j]**2>grid_tandem[i, j]):
             mu_star[i,j]=np.nan
-            secondOrderBound[i,j]=np.nan
-            c2Bound[i,j]=np.nan
+            TND[i,j]=np.nan
+            CmuTND[i,j]=np.nan
 
-
-# The following is not strictly essential, but it will eliminate
-# a warning.  Comment it out to see the warning.
-#mu_star = ma.masked_where(z <= 0, z)
-
-fig = plt.figure()
-plt.contourf(grid_gibbs_loss, grid_tandem_loss, mu_star, levels = 30, cmap='rainbow')
-plt.plot(expected_gibbs_loss, 0.5 * expected_gibbs_loss, c='black', label='$\mu^*=0$')
-plt.colorbar()
-plt.title('muStar')
-plt.xlabel('Expected Gibbs Loss')
-plt.ylabel('Expected Tandem Loss')
-plt.legend()
-plt.savefig(title+'muStar.png')
-
-fig = plt.figure()
-plt.contourf(grid_gibbs_loss, grid_tandem_loss, secondOrderBound, levels = 30, cmap = "rainbow")
-plt.colorbar()
-plt.title('TndBound')
-plt.xlabel('Expected Gibbs Loss')
-plt.ylabel('Expected Tandem Loss')
-plt.savefig(title+'TNDBound.png')
-
-fig = plt.figure()
-plt.contourf(grid_gibbs_loss, grid_tandem_loss, c2Bound, levels = 30, cmap = "rainbow")
-plt.colorbar()
-plt.title('mu-Bound')
-plt.xlabel('Expected Gibbs Loss')
-plt.ylabel('Expected Tandem Loss')
-plt.savefig(title+'muBound.png')
 
 plt.rcParams.update({
     'font.size': 30,
@@ -68,10 +36,8 @@ plt.rcParams.update({
 })
 
 fig = plt.figure(figsize=(12,9.6))
-#levels = np.linspace(0,1,11)
-plt.contourf(grid_gibbs_loss, grid_tandem_loss, c2Bound / secondOrderBound, levels = 30, cmap = "rainbow")
-plt.plot(expected_gibbs_loss, 0.5 * expected_gibbs_loss, c='black', linewidth=4, label=r"$\mathbb{E}_{\rho^2}[L(h,h')]=0.5\mathbb{E}_{\rho}[L(h)]$")
-#plt.plot(expected_gibbs_loss, 0.35 * expected_gibbs_loss, 'b-', label='tnd=0.35gibbs')
+plt.contourf(grid_gibbs, grid_tandem, CmuTND / TND, levels = 30, cmap = "rainbow")
+plt.plot(gibbs, 0.5 * gibbs, c='black', linewidth=4, label=r"$\mathbb{E}_{\rho^2}[L(h,h')]=0.5\mathbb{E}_{\rho}[L(h)]$")
 cbar = plt.colorbar()
 cbar.ax.tick_params(labelsize=40)
 cbar.ax.locator_params(nbins=8)
@@ -82,7 +48,7 @@ plt.ylabel(r"$\mathbb{E}_{\rho^2}[L(h,h')]$", fontsize=45)
 plt.yticks([0.1, 0.2, 0.3, 0.4], fontsize=42)
 plt.legend(loc='upper left', framealpha=0., handlelength=1, fontsize=40)
 plt.tight_layout()
-plt.savefig(title+'CmuT_vs_TND.png')
+plt.savefig('oracle_CmuT_vs_TND.png')
 
 
 
