ebb754b241bec54bd930416f5128db1d9ec52577
YiShanAngWu <yswu@di.ku.dk>
2021-10-28 16:12:28 +0200

    fix the comments

--- NeurIPS2021/artificialEvaluation/bennett_bound.py
+++ NeurIPS2021/artificialEvaluation/bennett_bound.py
@@ -1,6 +1,7 @@
 """
-We compare the tightness of the PAC-Bayes Bennett bound to the PAC-Bayes Bernstein bound.
-We consider only the gibbs loss and its variance, not the tandem loss. Thus, the random variables lie in [0,1].
+We compare the tightness of the PAC-Bayes-Bennett bound to the PAC-Bayes-Bernstein bound.
+We consider only the Gibbs loss and its variance, not the tandem loss.
+Thus, the random variable lies in [0,1].
 """
 
 import numpy as np
@@ -10,59 +11,66 @@ from math import log, sqrt, e, ceil
 
 
 """ Bernstein Bound:
-    Given the empirical gibbs and the variance, return the PAC-Bayes-Bernstein bound
+    Given the empirical loss and the variance, return the PAC-Bayes-Bernstein bound.
+    See Theorem 9 in the paper.
 """
-def PBBernstein_bound(emp_gibbs, var_bound, delta, kl, n):
-    # compute gam_star
-    a = min(var_bound, 1./4)
+def PBBernstein_bound(emp_loss, var, delta, kl, n):
+
+    a = min(var, 1./4) # Variance is bounded by 1/4
     b = (kl  + log(1/delta))/n
-    gam_star = sqrt(b / ((e-2)*a))
-    gam_star = min(gam_star, 1.)
     
-    return emp_gibbs + (e-2)*gam_star*a + b/gam_star
+    # Compute gam_star
+    gam_star = sqrt(b / ((e-2)*a))
+    gam_star = min(gam_star, 1.) # The range of gam_star is bounded
+    
+    return emp_loss + (e-2)*gam_star*a + b/gam_star
 
 """ Bennett Bound:
-    Given the empirical gibbs and the variance, return the PAC-Bayes-Bennett bound
+    Given the empirical loss and the variance, return the PAC-Bayes-Bennett bound.
+    See Theorem 9 in the paper.
 """
+
+# Calculate the coefficient of variance given gamma
 def VarCoeff(gam):
     return (e**gam - gam - 1) / gam
     
-def PBBen_bound(emp_gibbs, var, delta, kl, n):
-    # compute gam_star
-    a = min(var, 1./4)
+def PBBennett_bound(emp_loss, var, delta, kl, n):
+
+    a = min(var, 1./4) # Variance is bounded by 1/4
     b = (kl  + log(1/delta))/n
+    
+    # Compute gam_star
     c = 1/e * (b/a - 1)
     gam_star = 1.+lambertw(c, k=0).real
     
-    return emp_gibbs + VarCoeff(gam_star)*a + b/gam_star
+    return emp_loss + VarCoeff(gam_star)*a + b/gam_star
 
-# Define the eps term
+""" Define the complexity term """
 kl = 5
 delta = 0.05
 n = 10000
 
-""" construct the grid """
+""" Construct the grid """
 npoints = 200
-gibbs = np.logspace(-3.4, -0.4, npoints)
+loss = np.logspace(-3.4, -0.4, npoints)
 var = np.logspace(-3.4, -0.4, npoints)
-axis_gibbs, axis_var = np.meshgrid(gibbs, var) # axis
+axis_loss, axis_var = np.meshgrid(loss, var) # axis
 
-
-""" true variance """
-PBBen = np.zeros((npoints,npoints))
+""" Calculate the bounds """
+PBBennett = np.zeros((npoints,npoints))
 PBBernstein = np.zeros((npoints,npoints))
 
 for i in range(npoints):
     for j in range(npoints):
-        # if variance is larger than gibbs
-        if (axis_var[i,j]> axis_gibbs[i,j]):
-            PBBen[i,j] = np.nan
+        # if variance is larger than loss
+        if (axis_var[i,j]> axis_loss[i,j]):
+            PBBennett[i,j] = np.nan
             PBBernstein[i,j] = np.nan
         else:
-            PBBen[i,j] = PBBen_bound(axis_gibbs[i,j], axis_var[i,j], delta, kl, n)
-            PBBernstein[i,j] = PBBernstein_bound(axis_gibbs[i,j], axis_var[i,j], delta, kl, n)
+            PBBennett[i,j] = PBBennett_bound(axis_loss[i,j], axis_var[i,j], delta, kl, n)
+            PBBernstein[i,j] = PBBernstein_bound(axis_loss[i,j], axis_var[i,j], delta, kl, n)
 
-""" plot """
+""" Plot the ratio of PAC-Bayes-Bennett and PAC-Bayes-Bernstein """
 def plot():
     fig = plt.figure(figsize=(12,9.6))
     
@@ -72,7 +80,7 @@ def plot():
     'text.latex.preamble': r'\usepackage{amsfonts}'
     })
     
-    plt.contourf(axis_gibbs, axis_var, PBBen/PBBernstein, levels=25, cmap = "rainbow")
+    plt.contourf(axis_loss, axis_var, PBBennett/PBBernstein, levels=25, cmap = "rainbow")
     cbar = plt.colorbar()
     cbar.ax.tick_params(labelsize=40)
     cbar.ax.locator_params(nbins=5)
